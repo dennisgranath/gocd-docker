13b118bc670f0d814c6de058194b8ae0616f835f
developer <developer@sysco.com>
2015-12-01 09:29:26 -0600

    If group is not created in docker container, add docker group and assign existing gid

--- agent/add-go-user-to-docker-socket-group.sh
+++ agent/add-go-user-to-docker-socket-group.sh
@@ -2,4 +2,10 @@
 set -e
 
 DOCKER_SOCKET_GROUP=$(ls -l /var/run/docker.sock | awk '{print $4}')
+
+if [[ $DOCKER_SOCKET_GROUP =~ ^[0-9]+$ ]]; then
+  groupadd --gid $DOCKER_SOCKET_GROUP docker
+  DOCKER_SOCKET_GROUP=docker
+fi
+
 usermod --append --groups "$DOCKER_SOCKET_GROUP" go
