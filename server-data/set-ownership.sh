bfc306dc995bb356ec81508b82b8a739c081ea28
Andrew Shaw Care <andrewshawcare@gmail.com>
2015-12-13 21:20:57 -0600

    Add shebang

--- server-data/set-ownership.sh
+++ server-data/set-ownership.sh
@@ -1,1 +1,3 @@
-chown -R go:go /var/lib/go-server /var/log/go-server /etc/go /go-addons /var/go
+#!/bin/bash
+
+chown -R go:go /var/lib/go-server/addons /var/log/go-server /etc/go /go-addons /var/go
