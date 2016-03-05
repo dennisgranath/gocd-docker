237756b3390977f38ea5874a94879bfe0d36623a
Your Name <your@email.address>
2016-03-05 12:14:04 -0500

    Update to use docker-compose configuration version 2

--- setup.sh
+++ setup.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-# Create docker volumes
-docker volume create --name var_lib_go-server_addons
-docker volume create --name var_log_go-server
-docker volume create --name etc_go
-docker volume create --name go-addons
-docker volume create --name var_go
-
 export DOCKER_BINARY=$(which docker)
